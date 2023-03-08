bc9c42fe02373d62fd97a6bf05a7f2b46879c5dd
Krishna Pradhan <kpaccess@gmail.com>
2023-03-08 13:29:24 -0500

    coloring expense list items

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,10 +1,16 @@
 function Transaction({ id, amount, text }) {
+  const sign = amount < 0 ? '-' : '+';
+  console.log(sign);
+  const borderClr = sign === '-' ? 'border-red-600' : 'border-green-600';
   return (
     <li
       key={id}
-      className="px-5 py-1 bg-white  border-gray-500 grid grid-cols-2"
+      className={`mb-5 px-5 py-1 bg-white grid grid-cols-2 border-r-4 ${borderClr}`}
     >
-      {text} <span>-${amount}</span>
+      {text}{' '}
+      <span className=" ">
+        {sign}${Math.abs(amount)}
+      </span>
       <button className="hidden">x</button>
     </li>
   );
